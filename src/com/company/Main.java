5a7337de7acb8ab30856ad0cc8e64c80c10fcd0f
itpark <itpark.team@gmail.com>
2021-05-09 20:24:19 +0300

    do some math

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,50 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//        int a = 5;
+//        int b = 2;
+
+        //double a = 5;
+        //double b = 2;
+
+        //System.out.println(a/b);
+        //System.out.println(a/(b*1.0));
+        //System.out.println(a/(double)b);
+
+        //int a=123;
+        //System.out.println(a);
+
+        //a++;
+        //++a;
+        //a+=10;
+        //a=a+10;
+
+        //a--;
+        //--a;
+        //a-=10;
+        //a=a-10;
+
+        //a*=10;
+        //a=a*10;
+
+        //a/=10;
+        //a=a/10;
+
+        int a=123;
+        System.out.println(a);
+
+        /*
+        int b = a/1;//123
+        int b = a/10;//12
+        int b = a/100;//1
+        int b = a/1000;//0
+        */
+
+        //int b = 123 % 1;//0
+        //int b = 123 % 10;//3
+        //int b = 123 % 100;//23
+        int b = 123 % 1000;//123
+
+        System.out.println(b);
     }
 }
