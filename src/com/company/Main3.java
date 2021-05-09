44f87d7a12febedffe3cc82af6fec10ad442856a
itpark <itpark.team@gmail.com>
2021-05-09 21:00:31 +0300

    template for task3

---
+++ src/com/company/Main3.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main3 {
+    public static void main(String[] args) {
+        int a=3;
+        int b=5;
+
+        System.out.println("a = "+a+" b = "+b);
+        //todo swap
+        System.out.println("a = "+a+" b = "+b);
+    }
+}
