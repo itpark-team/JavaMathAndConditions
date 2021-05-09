86c41675794255412bf42b27f5339be0a94a63cd
itpark <itpark.team@gmail.com>
2021-05-09 21:17:20 +0300

    do swap

--- src/com/company/Main3.java
+++ src/com/company/Main3.java
@@ -2,15 +2,16 @@ package com.company;
 
 public class Main3 {
     public static void main(String[] args) {
-        int a=3;
-        int b=5;
 
-        System.out.println("a = "+a+" b = "+b);
+        int a = 3;
+        int b = 5;
+
+        System.out.println("a = " + a + " b = " + b);
 
         int c = a;
         a = b;
         b = c;
 
-        System.out.println("a = "+a+" b = "+b);
+        System.out.println("a = " + a + " b = " + b);
     }
 }
